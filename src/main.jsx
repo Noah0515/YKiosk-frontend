b73c051d8542b3a49c018c6b4ad411f52bf585c6
Yoon <01csyoon@gmail.com>
2025-12-23 02:02:07 +0900

    feat: route로 변경

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,13 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+import { BrowserRouter } from 'react-router-dom'
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    {/* 2. App을 감싸주기 */}
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </StrictMode>,
 )
