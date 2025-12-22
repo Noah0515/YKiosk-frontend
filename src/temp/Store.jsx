53619cba80cbdd35428c6904a49b78044ffa9e03
Yoon <01csyoon@gmail.com>
2025-12-23 05:58:10 +0900

    feat: 가게 기본화면 확인
    일단 전달받는 userName 출력 확인

--- src/temp/Store.jsx
+++ src/temp/Store.jsx
@@ -1,12 +1,15 @@
+import { useState} from 'react';
+import axios from 'axios'
+
 import { useNavigate, useOutletContext } from 'react-router-dom';
 
 function Store() {
     const {userName} = useOutletContext();
     return(
-        <div>
+        <div class="no-header-content">
             <p>{userName}</p>
         </div>
-    )
+    );
 }
 
 export default Store;
