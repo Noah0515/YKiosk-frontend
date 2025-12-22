2e4700fda29903e03a211af790ffbca1e387872e
Yoon <01csyoon@gmail.com>
2025-12-22 23:12:18 +0900

    feat: 로그인 후 api 테스트 완료
    이름 가져오기 성공

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import { useState } from 'react';
 import axios from 'axios';
+import Navbar from './default/Navbar';
 
 function App() {
-  const [userInfo, setUserInfo] = useState(null);
-
+  //const [userInfo, setUserInfo] = useState(null);
+  const [userName, setUserName] = useState(null);
+  
   // 1. 카카오 로그인 시작 (주소창 이동)
   const handleLogin = () => {
     // 백엔드 시큐리티 입구로 보냅니다.
@@ -14,11 +16,12 @@ function App() {
   const checkMyInfo = async () => {
     try {
       // 반드시 withCredentials: true 설정을 해야 브라우저가 쿠키를 같이 보냅니다.
-      const response = await axios.get("http://localhost:8080/YKiosk/api/user/me", {
+      const response = await axios.get("http://localhost:8080/YKiosk/api/user/name", {
         withCredentials: true 
       });
-      setUserInfo(response.data);
-      console.log("서버에서 받은 유저 정보:", response.data);
+      //setUserInfo(response.data);
+      setUserName(response.data.name)
+      console.log("서버에서 받은 유저 이름름:", response.data.name);
     } catch (error) {
       console.error("인증 실패 또는 로그인되지 않음", error);
       alert("로그인이 필요하거나 세션이 만료되었습니다.");
@@ -27,10 +30,12 @@ function App() {
 
   return (
     <div style={{ padding: '50px', textAlign: 'center', fontFamily: 'sans-serif' }}>
+      <Navbar userName={userName}/>
+
       <h1>Y-Kiosk Test Page</h1>
       <hr />
 
-      {!userInfo ? (
+      {!userName ? (
         <div style={{ marginTop: '20px' }}>
           <p>아직 로그인되지 않았습니다.</p>
           {/* 카카오 공식 디자인 색상 적용 */}
@@ -53,8 +58,7 @@ function App() {
       ) : (
         <div style={{ marginTop: '20px', border: '1px solid #ddd', padding: '20px' }}>
           <h3>🎉 로그인 성공!</h3>
-          <p><strong>아이디(Hash):</strong> {userInfo.userId}</p>
-          <p><strong>닉네임:</strong> {userInfo.name}</p>
+          <p><strong>닉네임:</strong> {userName}</p>
           <button onClick={() => setUserInfo(null)}>로그아웃(화면만)</button>
         </div>
       )}
