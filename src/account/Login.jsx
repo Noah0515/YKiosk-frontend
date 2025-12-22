2611c1b46825ef5e82b4d456e564a4ef0fe3c648
Yoon <01csyoon@gmail.com>
2025-12-23 02:03:51 +0900

    feat: 로그인 창 만들기
    layout은 향후 수정 필요. 기능은 정상작동.

---
+++ src/account/Login.jsx
@@ -0,0 +1,26 @@
+import '../Style.css'
+import './login.css'
+
+function Login() {
+    const handleLogin = () => {
+        // 백엔드 시큐리티 입구로 보냅니다.
+        window.location.href = "http://localhost:8080/YKiosk/oauth2/authorization/kakao";
+      };
+
+    return (
+        <div className="">
+            <div className="masthead bg-primary text-white text-center" >
+                <div className="d-flex align-items-center flex-column">
+                    <button onClick={handleLogin} className="kakao-login-btn"></button>
+                </div>
+                <div className="">
+
+                </div>
+            </div>
+        </div>
+        
+    )
+    
+}
+
+export default Login;
