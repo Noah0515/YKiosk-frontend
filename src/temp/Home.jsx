f7c51ce8933468f8c841acf563505d9c75212721
Yoon <01csyoon@gmail.com>
2025-12-23 02:09:44 +0900

    feat: 향후 쓸 홈 화면 컨텐츠
    일단 그냥 빈 화면으로 구성. 나중에 제대로 채우기

---
+++ src/temp/Home.jsx
@@ -0,0 +1,14 @@
+import {useState} from 'react'
+
+import axios from 'axios'
+
+function Home() {
+    return (
+        <div>
+            <p></p>
+        </div>
+    );
+}
+
+export default Home;
+
