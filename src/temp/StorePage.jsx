6990fcbb1bfeee37e10eab6e0e34380b85f8bf35
Yoon <01csyoon@gmail.com>
2025-12-25 04:14:13 +0900

    feat: 내가게 페이지 기본
    디자인은 안됨.

--- src/temp/StorePage.jsx
+++ src/temp/StorePage.jsx
@@ -1,13 +1,42 @@
-import { useState} from 'react';
-import axios from 'axios'
+import { useState, useEffect} from 'react';
+import axios from 'axios';
+import api from '../api';
 
 import { useNavigate, useOutletContext } from 'react-router-dom';
 
 function StorePage() {
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        const getStores = async () => {
+            try {
+                const stores = await api.get("/api/user/store/list", {
+
+                })
+            } catch (error) {
+
+            }
+        }
+    })
+
+    const goCreateStorePage = () => {
+        navigate('/my/store/create');
+    }
+
+
     const {userName} = useOutletContext();
     return(
         <div class="no-header-content">
             <p>{userName}</p>
+            <div>
+                <div>
+                    <div onClick={goCreateStorePage}>
+                        <h3>
+                            새로운 가게 만들기
+                        </h3>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
