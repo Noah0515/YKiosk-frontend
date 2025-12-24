6752c7ef26c81726d8192934cb3462b8928ff10b
Yoon <01csyoon@gmail.com>
2025-12-25 03:16:33 +0900

    refactor: api 요청을 base url을 쓰는 방식으로 교체
    로그인의 경우 만든 api함수를 안쓰고 base url만 사용

--- src/home/MainLayout.jsx
+++ src/home/MainLayout.jsx
@@ -4,26 +4,31 @@ import ReactDOM from 'react-dom/client'
 import axios from 'axios'
 import '../Style.css'
 import Navbar from '../default/Navbar'
-import Home from '../temp/Home';
-import Store from '../temp/Store';
+import HomePage from '../temp/HomePage';
+import StorePage from '../temp/StorePage';
+import api from '../api';
 
 function MainLayout() {
       //const [userInfo, setUserInfo] = useState(null);
   const [userName, setUserName] = useState(null);
+
+  const BASE_URL = import.meta.env.VITE_API_BASE_URL;
   
   // 1. 카카오 로그인 시작 (주소창 이동)
   const handleLogin = () => {
     // 백엔드 시큐리티 입구로 보냅니다.
-    window.location.href = "http://localhost:8080/YKiosk/oauth2/authorization/kakao";
+    window.location.href = "${BASE_URL}/oauth2/authorization/kakao";
   };
 
   useEffect(() => {
     const fetchUser = async () => {
       try {
         // 반드시 withCredentials: true 설정을 해야 브라우저가 쿠키를 같이 보냅니다.
+        /*
         const response = await axios.get("http://localhost:8080/YKiosk/api/user/name", {
           withCredentials: true 
-        });
+        });*/
+        const response = await api.get("api/user/name")
         //setUserInfo(response.data);
         setUserName(response.data.name)
         console.log("서버에서 받은 유저 이름:", response.data.name);
