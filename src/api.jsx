7f6b0561899eaf6cb38b60917739672a7f8eef7b
Yoon <01csyoon@gmail.com>
2025-12-25 03:14:41 +0900

    feat: base url 환경변수 설정
    restApi 요청하는 경우 이걸로 기본적(json만 쓰는 경우)으론 이걸로 요청.
    파일(이미지 등)이 포함되면 headers 설정 변경해서 해야됨.

---
+++ src/api.jsx
@@ -0,0 +1,12 @@
+import axios from 'axios';
+
+const apiURL = import.meta.env.VITE_API_BASE_URL;
+
+const api = axios.create({
+    baseURL: apiURL,
+    withCredentials: true, //쿠키 챙겨보내기
+});
+
+api.defaults.headers.common['Content-Type'] = 'applicatioin/json'
+export default api;
+
