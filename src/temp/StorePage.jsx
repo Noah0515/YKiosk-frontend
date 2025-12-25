320b14c3ed04896121a5a4c6eebbd050d6976ad0
Yoon <01csyoon@gmail.com>
2025-12-26 02:58:51 +0900

    feat: 가게 목록 띄우기
    디자인은 안됨

--- src/temp/StorePage.jsx
+++ src/temp/StorePage.jsx
@@ -3,21 +3,35 @@ import axios from 'axios';
 import api from '../api';
 
 import { useNavigate, useOutletContext } from 'react-router-dom';
+import StoreCard from './StoreCard';
 
 function StorePage() {
     const navigate = useNavigate();
+    const [stores, setStores] = useState([]);
 
     useEffect(() => {
         const getStores = async () => {
             try {
-                const stores = await api.get("/api/user/store/list", {
+                const response = await api.get("/api/user/store/list");
+                const storeList = response.data;
 
-                })
+                console.log("가게 리스트:" , storeList);
+                setStores(storeList);
             } catch (error) {
-
+                if (error.response) {
+                    // 서버가 응답을 보냈으나 2xx 범위를 벗어난 경우 (예: 401, 403, 500)
+                    console.error("에러 상태 코드:", error.response.status);
+                    if (error.response.status === 401) {
+                        alert("로그인이 만료되었습니다. 다시 로그인해주세요.");
+                    }
+                } else {
+                    // 요청 자체가 실패한 경우 (네트워크 오류 등)
+                    console.error("네트워크 에러 혹은 서버 응답 없음:", error.message);
+                }
             }
         }
-    })
+        getStores();
+    }, [])
 
     const goCreateStorePage = () => {
         navigate('/my/store/create');
@@ -26,15 +40,22 @@ function StorePage() {
 
     const {userName} = useOutletContext();
     return(
-        <div class="no-header-content">
+        <div className="no-header-content">
             <p>{userName}</p>
-            <div>
+            <div className="">
                 <div>
                     <div onClick={goCreateStorePage}>
                         <h3>
                             새로운 가게 만들기
                         </h3>
                     </div>
+                </div>
+                    {stores.map((store) => (
+                        <StoreCard key={store.storeId} store={store} />
+                    ))
+                    }
+                <div>
+                    
                 </div>
             </div>
         </div>
