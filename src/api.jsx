23f3d19c710f2c2e1b42f373b35b23f514ca3a55
Yoon <01csyoon@gmail.com>
2025-12-31 13:40:41 +0900

    feat: 인터셉터 추가

--- src/api.jsx
+++ src/api.jsx
@@ -7,6 +7,25 @@ const api = axios.create({
     withCredentials: true, //쿠키 챙겨보내기
 });
 
+// ★ 요청 인터셉터 추가
+api.interceptors.request.use(
+    (config) => {
+        // 로컬 스토리지에서 토큰을 가져옵니다. (저장하신 키 이름 확인 필요)
+        const token = localStorage.getItem("accessToken"); 
+        
+        if (token) {
+            // 헤더에 Bearer 토큰 추가
+            config.headers.Authorization = `Bearer ${token}`;
+        }
+        return config;
+    },
+    (error) => {
+        return Promise.reject(error);
+    }
+);
+
+api.defaults.headers.common['Content-Type'] = 'application/json';
+
 api.defaults.headers.common['Content-Type'] = 'application/json'
 export default api;
 
